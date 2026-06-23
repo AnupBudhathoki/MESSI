1e074bb11d1fae5699d2bd87134b2a3df43b2c8c
AnupBudhathoki <kshetria656@gmail.com>
2026-06-23 14:23:34 +0545

    Host

--- Messi/settings.py
+++ Messi/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 DEBUG = ("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["messi-nvpn.onrender.com"]
 
 
 # Application definition
