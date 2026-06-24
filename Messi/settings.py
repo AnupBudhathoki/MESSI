8f04fe867d16875c688a232fb2b024f09b342e53
AnupBudhathoki <kshetria656@gmail.com>
2026-06-24 08:19:43 +0545

    localhost update

--- Messi/settings.py
+++ Messi/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 DEBUG = config("DEBUG",cast=bool)
 
-ALLOWED_HOSTS = ["messi-nvpn.onrender.com", "http://127.0.0.1:8000/"]
+ALLOWED_HOSTS = ["messi-nvpn.onrender.com", "127.0.0.1", "localhost",]
 
 
 # Application definition
